330d16947cdbf170709e3b3a259216ee45ab6f60
somefoo <50917034+somefoo@users.noreply.github.com>
2021-10-11 18:49:52 +0200

    Cleaned up code generation script 	renaming/comments/removed old code

--- addons/sofa_scene_creator/xml_scene_tree.gd
+++ addons/sofa_scene_creator/xml_scene_tree.gd
@@ -82,6 +82,7 @@ func add_child(node, type = "Node"):
 func add_property(node, name, value):
 	return node.add_property(name, value)
 
+# Creates an XML description of the scene
 func to_xml():
 	return to_xml_from_node(_root)
 func to_xml_from_node(node, depth = 0):
@@ -107,7 +108,7 @@ func to_xml_from_node(node, depth = 0):
 
 
 
-
+# Creates a python3 description of the scene 
 func to_python3():
 	# Do stuff for root only (like the createScene call)
 	var root_content = "def createScene(root_node):\n"
@@ -117,117 +118,28 @@ func to_python3():
 		root_content += "\troot_node." + property_name + ' = ' + str(property_value) + '\n'
 	var sofa_root = "\treturn root_node\n"
 	return root_content + to_python3_from_node("root_node",_root) + sofa_root
+	
 func to_python3_from_node(parent_name,node, depth = 0):
 	var properties_string = ""
 	for p in range(0, node._properties.size()):
 		var property_value = node._properties.values()[p]
 		properties_string += node._properties.keys()[p] + '=' + _value_to_string(property_value) + ', '
 
-	var xml_string = ''
+	var python_string = ''
 	var prefix = '\t'.repeat(depth)
 	if(node._children.size() == 0):
-		xml_string += prefix + parent_name + ".addObject(" + '"' + node._type + '", ' + properties_string + ')'
+		python_string += prefix + parent_name + ".addObject(" + '"' + node._type + '", ' + properties_string + ')'
 	else:
 		# Make sure that the root node is not created as a child 
 		if(depth > 0):
-			xml_string += prefix + node._properties["name"] + " = " + parent_name + ".addChild(" + '"' + node._type + '", ' + properties_string + ')'
-			xml_string += '\n'
-			xml_string += prefix + 'def add():\n'
+			python_string += prefix + node._properties["name"] + " = " + parent_name + ".addChild(" + '"' + node._type + '", ' + properties_string + ')'
+			python_string += '\n'
+			python_string += prefix + 'def add():\n'
 			for child in node._children:
-				xml_string += to_python3_from_node(node._properties["name"],child, depth + 1)
-			xml_string += prefix + 'add()'
+				#TODO Will _properties["name"] always exist?
+				python_string += to_python3_from_node(node._properties["name"],child, depth + 1)
+			python_string += prefix + 'add()'
 		else:
 			for child in node._children:
-				xml_string += to_python3_from_node(parent_name,child, depth + 1)
-			
-	#var xml_string = '<?xml version="1.0" ?>'
-	return xml_string + '\n'
-
-#class_name SofaXMLNode
-
-#var _data = {"___type":"Node"}
-#
-#func set_type(type):
-#	_data["___type"] = type
-#
-#func add_property(name, value):
-#	assert(name.find('___') != 0, "Error, ___ is a reserved prefix!")
-#
-#	_data[name] = value
-#
-#	#_properties[name] = str(value)
-#
-#func add_child(type = "Node"):
-#	#Godot does not allow recursive type definition, this is a work-around
-#	var child = "___child" + str(_data.size())
-#	_data[child] = {"___type":"Node"}
-#	return _data[child]
-#
-#func to_xml(depth = 0):
-#	return _to_xml(_data)
-#
-#func _to_xml(data, depth = 0):
-#	var properties_string = ""
-#	for p in range(0, _data.size()):
-#		if(_data.keys()[p].find('___') != 0):
-#			properties_string += data.keys()[p] + ' = "' + data.values()[p] + '" '
-#
-#
-#	var xml_string = ''
-#	var prefix = ' '.repeat(depth)
-#
-#	var child_count = 0
-#	for c in range(0, data.size()):
-#		if(data.keys()[c].find('___child') == 0):
-#			child_count += 1
-#
-#
-#	if(child_count == 0):
-#		xml_string += prefix + '<' + data['___type'] + ' ' + properties_string + '/>'
-#	else:
-#		xml_string += prefix + '<' + data['___type'] + ' ' + properties_string + '>'
-#		xml_string += '\n'
-#		for c in range(0, data.size()):
-#			if(data.keys()[c].find('___child') == 0):
-#				xml_string += _to_xml(data.keys()[c], depth + 1)
-#		xml_string += prefix + '</' + data['___type'] + '>'
-#	#var xml_string = '<?xml version="1.0" ?>'
-#	return xml_string + '\n'
-
-
-#class_name SofaXMLNode
-#
-#var _children = []
-#var _type = "Node"
-#var _properties = {}
-#
-#var _info = {}
-#
-#func set_type(type):
-#	_type = type
-#
-#func add_property(name, value):
-#	_properties[name] = str(value)
-#
-#func add_child(type = "Node"):
-#	#_children.append(SofaXMLNode.new())
-#	pass
-#
-#func to_xml(depth = 0):
-#	var properties_string = ""
-#	for p in range(0, _properties.size()):
-#		properties_string += _properties.keys()[p] + ' = "' + _properties.values()[p] + '" '
-#
-#
-#	var xml_string = ''
-#	var prefix = ' '.repeat(depth)
-#	if(_children.size() == 0):
-#		xml_string += prefix + '<' + _type + ' ' + properties_string + '/>'
-#	else:
-#		xml_string += prefix + '<' + _type + ' ' + properties_string + '>'
-#		xml_string += '\n'
-#		for child in _children:
-#			xml_string += child.to_xml(depth + 1)
-#		xml_string += prefix + '</' + _type + '>'
-#	#var xml_string = '<?xml version="1.0" ?>'
-#	return xml_string + '\n'
+				python_string += to_python3_from_node(parent_name,child, depth + 1)
+	return python_string + '\n'
